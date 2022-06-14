1e89dd3db0892e6f7594dfa8a803cfc5f0c01bb6
leonardocloliver <leonardocloliver@gmail.com>
2022-06-14 16:02:32 -0300

    refactor: add pylint django

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect 
-from chat.models import Room, Message
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-
-
+from chat.models import Room, Message
 
 # Create your views here.
 def home(request):
@@ -14,16 +12,16 @@ def room(request, room):
     return render(request, 'room.html', {
         'username': username,
         'room': room,
-        'room_details': room_details 
+        'room_details': room_details
     })
-   
+
 def checkview(request):
     room = request.POST['room_name']
     username = request.POST['username']
 
     if Room.objects.filter(name=room).exists():
         return redirect('/'+room+'/?username='+username)
-    
+
     new_room = Room.objects.create(name=room)
     new_room.save()
     return redirect('/'+room+'/?username='+username)
@@ -35,7 +33,7 @@ def send(request):
 
     new_message = Message.objects.create(value=message, user=username, room = room_id)
     new_message.save()
-    return HttpResponse('Message sent sucessfuly')        
+    return HttpResponse('Message sent sucessfuly')
 
 def getMessages(request, room):
     room_details = Room.objects.get(name=room)
